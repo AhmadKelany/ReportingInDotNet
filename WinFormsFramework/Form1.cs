b142bb42cae669ee285a60bb33353dcf9786929a
Ahmad Kelany <ahmad.kelany@yahoo.com>
2022-09-28 14:57:12 +0300

    after 02-01 exploring winforms targeting .net framework

--- WinFormsFramework/Form1.cs
+++ WinFormsFramework/Form1.cs
@@ -16,5 +16,11 @@ namespace WinFormsFramework
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
